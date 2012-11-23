1e4c5b3420dfb5c417f2dfa4f1708bc271586ac3
Aaron Diamantberg <ivan.isaacs@windowslive.com>
2012-11-23 19:25:45 +0100

    moved main code in init-function

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, 650/365, 0.1, 1000);
-var container = $('#inmidstofwar');
+function init() {
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(75, 650/365, 0.1, 1000);
+	var container = $('#inmidstofwar');
 
-var renderer = new THREE.WebGLRenderer();
+	var renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(650, 365);
+	renderer.setSize(650, 365);
 
-container.append(renderer.domElement);
+	container.append(renderer.domElement);
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+	var geometry = new THREE.CubeGeometry(1,1,1);
+	var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+	var cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
 
-camera.position.z = 5;
+	camera.position.z = 5;
+
+	render();
+}
 
 function render() {
 	requestAnimationFrame(render);
 	cube.rotation.x += 0.1; cube.rotation.y += 0.1;
 	renderer.render(scene, camera);
 }
-
-render();
